9c6f1ac5c6d65d6222c334eae3b61720958798ff
Ando <ando9527@gmail.com>
2018-05-31 14:51:02 +0800

    fixed wob

--- src/reducer/wob.js
+++ src/reducer/wob.js
@@ -14,13 +14,14 @@ export function wobReducer(state = null, action) {
 }
 
 export const wobMaker=({state, orderBook})=>{
-  console.log(state);
-  console.log(orderBook);
+  // console.log(state);
+  // console.log(orderBook);
   
   
   const {asks, bids} = state
   const {asks: uasks, bids: ubids} = orderBook
   let newAsks = []
+  if (uasks.length===0) newAsks = asks
   for (let item of asks){
     for (let j of uasks){
       if(item.price===j.price){
@@ -33,6 +34,7 @@ export const wobMaker=({state, orderBook})=>{
     }
   }     
   let newBids = []
+  if (ubids.length===0) newBids = bids
   for (let item of bids){
     for (let j of ubids){
       if(item.price===j.price){
@@ -40,11 +42,12 @@ export const wobMaker=({state, orderBook})=>{
         const size = item.size+j.size
         if(count!==0) newBids.push(Object.assign({},{price: item.price, count, size}))
       }else{
+        
         newBids.push(item)
       }
     }
   }  
-
+  
   return Object.assign({}, {asks:newAsks, bids: newBids})
 
 }
