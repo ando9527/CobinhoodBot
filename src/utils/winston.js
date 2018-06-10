c4f454829b2886e0068addad32d2709fe6848a9c
Ando <ando9527@gmail.com>
2018-06-10 17:14:26 +0800

    remove m sec

--- src/utils/winston.js
+++ src/utils/winston.js
@@ -3,7 +3,7 @@ import winston from 'winston'
 import moment from 'moment-timezone'
 dotenv.load()
 const env = process.env.NODE_ENV || 'development';
-const tsFormat = () => moment().tz("Asia/Taipei").format('YYYY-MM-DD HH:mm:ss.SSSS');
+const tsFormat = () => moment().tz("Asia/Taipei").format('YYYY-MM-DD HH:mm:ss');
 const logger = new (winston.Logger)({
   transports: [
     // colorize the output to the console
