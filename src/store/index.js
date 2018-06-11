8ce09265204bb247496708185a064e2f128628a3
Ando <ando9527@gmail.com>
2018-06-11 17:03:54 +0800

    remove none use reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,14 @@
 import { createStore, combineReducers } from 'redux'
-import {balanceReducer} from './balance'
 import {buyOrderReducer} from './buyOrder'
 import {opPriceReducer} from './opPrice'
 import {sellOrderReducer} from './sellOrder'
-import {orderIdReducer} from './orderId'
 import {wobReducer} from './wob'
 
 const rootReducer = combineReducers({
     buyOrder: buyOrderReducer,
-    balance: balanceReducer,
     orderBook: wobReducer,
     opPrice: opPriceReducer,
     sellOrder: sellOrderReducer,
-    orderId: orderIdReducer,
   });
 const store = createStore(rootReducer)
 
